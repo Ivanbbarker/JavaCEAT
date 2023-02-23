c7aaaa92fb1fb9dcd6ba951b395282f210bfc5df
Ivan <72516268+Ivanbbarker@users.noreply.github.com>
2023-02-23 18:02:19 +0100

    Create ejercicioPropuesto5_3.java

---
+++ ejercicioPropuesto5_3.java
@@ -0,0 +1,36 @@
+package tema5;
+
+import java.util.Arrays;
+
+public class ejercicio5_7 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] repetidos(int[] t) {
+        int temporal[] = new int[10];
+
+        for (int i : t) {
+            if (buscar(temporal, i) == -1) {
+                temporal = Arrays.copyOf(temporal, temporal.length + 1);
+                temporal[temporal.length - 1] = i;
+            }
+        }
+
+        return temporal;
+    }
+/* 
+  ACTIVIDAD PROPUESTA 5.3
+    */ 
+    public static int buscar(int t[], int clave) {
+        int numero = -1;
+        for (int i : t) {
+            if (t[i] == clave) {
+                numero = clave;
+            }
+        }
+        return numero;
+    }
+
+}
