924e789b472a4a04f52af91a661c139f798bcfe0
Ivan <72516268+Ivanbbarker@users.noreply.github.com>
2023-03-02 19:38:41 +0100

    Update ejercicio5_20Divide.java

--- ejercicio5_20Divide.java
+++ ejercicio5_20Divide.java
@@ -1,91 +1,112 @@
-package ejercicio5_19;
+/*
+ * La fusion de dos tablas ordenadas consiste en copiar todos sus elementos ( de 
+ * ambas tablas) en una tercera que deberá seguir ordenada. Podemos realizar una
+ * fusión <<ineficiente>> copiando los elementos de ambas tablas(sin tener en 
+ * cuenta el orden) en la tabla final y ordenar esta. Existe una manera óptima  
+ * de realizar la fusion optima de realizar la fusion en la que se eleige en 
+ * cada momento el primer elemento no copiado de alguna de las tablas y se añade
+ * de la tabla final, que seguirá ordenada sin necesidad de ordenacion alguna.
+ * Busca informacion sobre el algoritmo de fusion e implementalo en JAVA.
+ */
+package ejercicio5_20;
 
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Ejercicio5_19 {
+public class Ejercicio5_20 {
 
     public static void main(String[] args) {
-        int t[] = llegadaCorredores();
-        System.out.println(Arrays.toString(t));
-        t = dopados(t);
-        System.out.println(Arrays.toString(t));
-    }
-
-    public static int[] llegadaCorredores() {
-        int copiaT[] = new int[0];
-        Scanner sc = new Scanner(System.in);
-        int dorsal;
-        do {
-            System.out.print("Introduce el dorsal del participante: ");
-            dorsal = sc.nextInt();
-            if (dorsal != -1) {
-                copiaT = Arrays.copyOf(copiaT, copiaT.length + 1);
-                copiaT[copiaT.length - 1] = dorsal;
-                System.out.println("Dorsal: " + dorsal + ", Posicion: " + (copiaT.length - 1));
-            }
-        } while (dorsal != -1);
-        return copiaT;
-    }
-
-    public static void mostrarGanadores(int t[]) {
-        for (int i = 0; i < 3; i++) {
-            switch (i) {
-                case 0 ->
-                    System.out.println("Medalla de ORO, DORSAL:" + t[i]);
-                case 1 ->
-                    System.out.println("Medalla de PLATA, DORSAL: " + t[i]);
-                case 3 ->
-                    System.out.println("Medalla de BRONCE, DORSAL:" + t[i]);
-            }
+        /**
+         * Pedimos la longitud y creamos los arrays unidimensionales de nombre
+         * "t[]" y "x[]"
+         */
+        int t[] = new int[pedirLongitudArray()];
+        t = crearArray(t);
+        int x[] = new int[pedirLongitudArray()];
+        x = crearArray(x);
+        /**
+         * Creamos un array unidimensional donde uniremos las dos tablas
+         * ordenadas, en una que no lo estará, de nombre "desordenadaT[]"
+         */
+        int desordenadaT[] = new int[(t.length + x.length)];
+        desordenadaT = unirDesordenada(t, x);
+        //Aplicamos el algoritmo divide y venceras (recursivo)
+        divideVenceras(desordenadaT, 0, desordenadaT.length - 1);
+        //Mostramos el array ordenado
+        System.out.print("[ ");
+        for (int i = 0; i < desordenadaT.length; i++) {
+            System.out.print(desordenadaT[i] + ", ");
         }
+        System.out.print("]");
     }
 
-    public static int[] dopados(int t[]) {
+    public static int pedirLongitudArray() {
         Scanner sc = new Scanner(System.in);
-        //System.out.println("(introduce -1 para parar de introducir dorsales)");
-        int dorsal;
-        do {
-            System.out.print("Introduce el dorsal del participante dopado: ");
-            dorsal = sc.nextInt();
-            int indiceBusqueda = 0;
-            while (indiceBusqueda < t.length && t[indiceBusqueda] != dorsal) {
-                indiceBusqueda++;
-            }
-            if (indiceBusqueda == t.length) {
-                System.out.println("Ese dorsal no se encuentra en la carrera.");
-            } else {
-                t = Arrays.copyOf(t, t.length + 1);
-                for (int i = indiceBusqueda; i < t.length - 1; i++) {
-                    t[i] = t[i+1];
-                }
-                t = Arrays.copyOf(t, t.length - 2);
-                
-            }
-        } while (dorsal != -1);
+        System.out.print("\nIntroduce la longitud que desees: ");
+        int numero = sc.nextInt();
+        return numero;
+    }
+
+    public static int[] crearArray(int t[]) {
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < t.length; i++) {
+            System.out.print("\nIntroduce el valor que desees: ");
+            t[i] = sc.nextInt();
+        }
+
         return t;
     }
 
-    public static void dopadosv2(int t[]) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("(introduce -1 para parar de introducir dorsales)");
-        int dorsal;
-        do {
-            System.out.print("Introduce el dorsal del participante dopado: ");
-            dorsal = sc.nextInt();
-            int indiceBusqueda = 0;
-            while (indiceBusqueda < t.length && t[indiceBusqueda] != dorsal) {
-                indiceBusqueda++;
-            }
-            if (indiceBusqueda < t.length) {
-                t = Arrays.copyOf(t, t.length + 1);
-                t[t.length - 1] = t[indiceBusqueda];
-                t = Arrays.copyOfRange(t, indiceBusqueda, t.length + 1);
-            } else {
-                System.out.println("Ese dorsal no se encuentra en la carrera.");
-            }
-        } while (dorsal != -1);
+    public static int[] unirDesordenada(int t[], int x[]) {
+        int arrayEntero[] = new int[0];
 
+        for (int i = 0; i < t.length; i++) {
+            arrayEntero = Arrays.copyOf(arrayEntero, arrayEntero.length + 1);
+            arrayEntero[arrayEntero.length - 1] = t[i];
+        }
+        for (int i = 0; i < x.length; i++) {
+            arrayEntero = Arrays.copyOf(arrayEntero, arrayEntero.length + 1);
+            arrayEntero[arrayEntero.length - 1] = x[i];
+        }
+        return arrayEntero;
     }
 
+    public static void divideVenceras(int[] t, int izquierda, int derecha) {
+        if (izquierda < derecha) {
+            int medio = (izquierda + derecha) / 2;
+            divideVenceras(t, izquierda, medio);
+            divideVenceras(t, medio + 1, derecha);
+            unirOrdenada(t, izquierda, medio, derecha);
+        }
+    }
+
+    public static void unirOrdenada(int[] t, int izquierda, int medio, int derecha) {
+        int[] arrayOrdenado = new int[t.length];
+        int inicioDerecha = medio + 1;
+        int temporal = izquierda;
+        int indice = izquierda;
+        // Compare el tamaño de matriz de las posiciones de subíndice 
+        //correspondientes de las dos matrices pequeñas, colocando primero las 
+        //pequeñas en la nueva matriz
+        while (izquierda <= medio && inicioDerecha <= derecha) {
+            if (t[izquierda] <= t[inicioDerecha]) {
+                arrayOrdenado[indice++] = t[izquierda++];
+            } else {
+                arrayOrdenado[indice++] = t[inicioDerecha++];
+            }
+        }
+        // Si hay datos a la izquierda que deben copiarse, 
+        //copia la matriz restante de la izquierda a la nueva matriz ordenda
+        while (izquierda <= medio) {
+            arrayOrdenado[indice++] = t[izquierda++];
+        }
+        // Si hay datos a la derecha que deben copiarse, 
+        // copia la matriz restante de la derecha a la nueva matriz ordenada
+        while (inicioDerecha <= derecha) {
+            arrayOrdenado[indice++] = t[inicioDerecha++];
+        }
+        while (temporal <= derecha) {
+            t[temporal] = arrayOrdenado[temporal++];
+        }
+    }
 }
