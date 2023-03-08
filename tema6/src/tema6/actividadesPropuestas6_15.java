324cad0a4140541d9626c3decfabc1a83832ab2c
Ivan <72516268+Ivanbbarker@users.noreply.github.com>
2023-03-08 19:07:55 +0100

    Update actividadesPropuestas6_15.java

--- tema6/src/tema6/actividadesPropuestas6_15.java
+++ tema6/src/tema6/actividadesPropuestas6_15.java
@@ -6,12 +6,30 @@ package tema6;
  */
 public class actividadesPropuestas6_15 {
 
-    public static void construirEncabezado(String encabezado) {
-        System.out.println("<h1 " + encabezado + " </h1>");
-    }
 
-    public static void construirParrafo(String parrafo) {
-        System.out.println("<p> " + parrafo + " </p>");
+    public static void construirEncabezado(String encabezado, String parrafo) {
+        System.out.println("<DOCTYPE!>\n<html>\n\t<head>\n\t\t<title><h1 " + encabezado + " </h1></title>\n\t</head>"); 
+        System.out.println("\t<body>\n\t\t<p> " + parrafo + " </p>\n\t</body>\n</html>");
+    }
+    
+    public static String pedirEncabezado(){
+        String encabezado;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduce el encabezado: ");
+        encabezado = sc.nextLine();
+        return encabezado;
+    }
+    
+     public static String pedirParrafo(){
+        String parrafo;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduce el parrafo: ");
+        parrafo = sc.nextLine();
+        return parrafo;
+    }
+    
+    public static void main(String[] args) {
+        construirEncabezado(pedirEncabezado(), pedirParrafo());
     }
 
 }
