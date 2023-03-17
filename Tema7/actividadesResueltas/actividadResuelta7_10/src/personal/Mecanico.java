82b246d1ed9451ebfcb6e2f9dcb5bd4be1c30fc1
Ivan <72516268+Ivanbbarker@users.noreply.github.com>
2023-03-17 19:58:15 +0100

    Add files via upload

---
+++ Tema7/actividadesResueltas/actividadResuelta7_10/src/personal/Mecanico.java
@@ -0,0 +1,13 @@
+package personal;
+public class Mecanico {
+    String nombre;
+    String telefono;
+    enum Especialidad{FRENOS, HIDRAULICA, ELECTRICIDAD, MOTOR};
+    Especialidad especialidad;
+    
+    public Mecanico(String nombre, String telefono, String especialidad) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.especialidad = Especialidad.valueOf(especialidad);
+    }
+}
