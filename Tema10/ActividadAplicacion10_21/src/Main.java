0a37d4afc153b65b945ec36552178208d3a23282
Ivan <72516268+Ivanbbarker@users.noreply.github.com>
2023-04-13 19:17:28 +0200

    Add files via upload

--- Tema10/ActividadAplicacion10_21/src/Main.java
+++ Tema10/ActividadAplicacion10_21/src/Main.java
@@ -12,38 +12,31 @@ public class Main {
         BufferedWriter escritura = null;
         BufferedReader bre = null;
         FileInputStream flujo = null;
-        Scanner sc = new Scanner(System.in);
-        String contactos[] = new String[0];
+        Scanner sc;
+
         int opcion;
         boolean salir = false;
         do {
             menu();
             System.out.print("\nIntroduce la opcion que deseas: ");
+            sc = new Scanner(System.in);
             opcion = sc.nextInt();
             switch (opcion) {
                 case 1:
+                    String contactos[] = new String[0];
                     String texto;
                     System.out.println("1. Agregar Contacto\n");
                     try {
-                        BufferedReader br = new BufferedReader(new FileReader("agenda.txt"));
-                        sc = new Scanner(br);
-                        while (sc.hasNext()) {
-                            String contacto = sc.nextLine();
-                            contactos = Arrays.copyOf(contactos, contactos.length + 1);
-                            contactos[contactos.length - 1] = contacto;
-                        }
-                        //
-                        System.out.println(Arrays.toString(contactos));
+                        contactos = leerFichero(bre, sc);
                         do {
                             sc = new Scanner(System.in);
                             System.out.print("Introduce el nombre del contacto: ");
                             texto = sc.next();
                             System.out.print("Introduce el numero de telefono del contacto: ");
-                            texto = texto + " " + sc.next();
+                            texto = texto + "     " + sc.next();
                         } while (existeFirma(contactos, texto));
                         //
                         escritura = new BufferedWriter(new FileWriter("agenda.txt", true));
-                        sc = new Scanner(System.in);
                         escritura.write(texto);
                         escritura.newLine();
                     } catch (IOException e) {
@@ -59,30 +52,18 @@ public class Main {
                     }
                     break;
                 case 2:
-
+                        // Guardamos los datos del archivo agenda en un array de tipo String para poder
+                        // compararlo con la busqueda
+                        contactos = leerFichero(bre, sc);
+                        // Pedir dato de busqueda
+                        System.out.print("Introduce tu busqueda: ");
+                        sc = new Scanner(System.in);
+                        String busqueda = sc.next();
+                        mostrarContactos(contactos, busqueda);
                     break;
                 case 3:
-                    String agendaCompleta = "";
-                    try {
-                        bre = new BufferedReader(new FileReader("agenda.txt"));
-                        String linea = bre.readLine();
-                        while (linea != null) {
-                            agendaCompleta = agendaCompleta + linea + '\n';
-                            linea = bre.readLine();
-                        }
-                        System.out.println("Nombre | Numero");
-                        System.out.println(agendaCompleta);
-                    } catch (IOException e) {
-                        System.out.println(e.getMessage());
-                    } finally {
-                        if (bre != null) {
-                            try {
-                                bre.close();
-                            } catch (IOException e) {
-                                System.out.println(e);
-                            }
-                        }
-                    }
+                        contactos =leerFichero(bre, sc);
+                        mostrarAgendaAlpha(contactos);
                     break;
                 case 4:
                     System.out.println("Saliendo...");
@@ -109,5 +90,53 @@ public class Main {
             }
         }
         return existe;
+    }
+
+    public static void mostrarContactos(String[] contactos, String busqueda) {
+        System.out.println();
+        for (int i = 0; i < contactos.length; i++) {
+            int coincidencias = 0;
+            for (int j = 0; j < busqueda.length(); j++) {
+                String contacto = contactos[i].toLowerCase();
+                if (contacto.charAt(j) == busqueda.toLowerCase().charAt(j)) {
+                    coincidencias++;
+                    if (coincidencias == busqueda.length()) {
+                        System.out.println(contacto);
+                    }
+                }
+            }
+        }
+    }
+
+    public static void mostrarAgendaAlpha(String[] contactos) {
+        Arrays.sort(contactos);
+        for (int i = 0; i < contactos.length; i++) {
+            System.out.println(contactos[i]);
+        }
+    }
+
+    public static String[] leerFichero(BufferedReader br, Scanner sc) {
+        String[] contactos = new String[0];
+        try {
+            br = new BufferedReader(new FileReader("agenda.txt"));
+            sc = new Scanner(br);
+            while (sc.hasNext()) {
+                String contacto = sc.nextLine();
+                contactos = Arrays.copyOf(contactos, contactos.length + 1);
+                contactos[contactos.length - 1] = contacto;
+            }
+            sc.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    System.out.println(e);
+                }
+            }
+        }
+        return contactos;
     }
 }
